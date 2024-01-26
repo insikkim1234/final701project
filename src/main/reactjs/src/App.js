b3bb7e8fa57d88b813dd3334472a21d5c650a6c8
KIMINSIK <insikkim2439@gmail.com>
2024-01-26 16:10:41 +0900

    first

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import mainImg from'./image/muzidan.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={mainImg}/>
+      <h2><b>스프링부트 cicd 성공기원 한번에 성공하기</b></h2>
+    </div>
+  );
+}
+
+export default App;
